9402c6ba185503c1e779a6ec2792f524950635bd
Ray-Shakex <bshakex@gmail.com>
2021-06-21 11:51:19 +0200

    footer p font size media query

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 const date = document.getElementById('date');
 
-date.innerHTML = new Date().getFullYear();
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navigation.classList.toggle('active');
 });
 
+date.innerHTML = new Date().getFullYear();
