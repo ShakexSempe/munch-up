7350f4a9ee6b2e5df7cc8fce9945aa57a5bc6f43
Ray-Shakex <bshakex@gmail.com>
2021-10-21 12:21:19 +0200

    home main.js test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
+const home = document.querySelector('.home-content');
+const homeImg = document.querySelector('.home-img');
+
 const date = document.getElementById('date');
 
+console.log(home);
+
+document.addEventListener("scroll", () => {
+    let scroll_position = window.scrollY;
+    let scroll_height = window.pageYOffset;
+
+    if(scroll_position < 20 ) {
+        home.style.opacity = 0;
+        homeImg.style.opacity = 1;
+        
+    } else {
+        home.style.opacity = 1;
+        homeImg.style.opacity = 0;
+    }
+});
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
