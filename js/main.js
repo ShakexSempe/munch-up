e3c36c0d4673fd9d812b64351ac59a35a3e87fed
Ray-Shakex <bshakex@gmail.com>
2022-03-05 17:12:52 +0200

    #home style

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ const date = document.getElementById('date');
 
 console.log(home);
 
-document.addEventListener("scroll", () => {
-    let scroll_position = window.scrollY;
-    let scroll_height = window.pageYOffset;
+// document.addEventListener("scroll", () => {
+//     let scroll_position = window.scrollY;
+//     let scroll_height = window.pageYOffset;
 
-    if(scroll_position < 20 ) {
-        home.style.opacity = 0;
-        homeImg.style.opacity = 1;
+//     if(scroll_position < 20 ) {
+//         home.style.opacity = 0;
+//         homeImg.style.opacity = 1;
         
-    } else {
-        home.style.opacity = 1;
-        homeImg.style.opacity = 0;
-    }
-});
+//     } else {
+//         home.style.opacity = 1;
+//         homeImg.style.opacity = 0;
+//     }
+// });
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
