79942a301ec31f42dfe7805e942d4035ea89e59d
Ray-Shakex <bshakex@gmail.com>
2023-09-02 13:54:05 +0200

    removed io-item links

--- js/main.js
+++ js/main.js
@@ -1,53 +1,92 @@
+// VARIABLES
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
-// const home = document.querySelector('.home-content');
 const homeImg = document.querySelector('.home-img');
 const logo = document.querySelector(".logo");
 const HEADER = document.querySelector(".header");
+const navLinks = document.querySelectorAll('.header a');
 
 const date = document.getElementById('date');
 const pageLinks = document.querySelectorAll('.page-link');
-console.log(pageLinks);
 const activePage = window.location.pathname;
+
+// ACTIVE PAGE LOGIC
 pageLinks.forEach(link => {
     if (link.href.includes(`${activePage}`)) {
         link.classList.add('active');
         console.log(`${activePage} is active`)
     }
 });
-// document.addEventListener("scroll", () => {
-//     let scroll_position = window.scrollY;
-//     let scroll_height = window.pageYOffset;
 
-//     if(scroll_position < 20 ) {
-//         home.style.opacity = 0;
-//         homeImg.style.opacity = 1;
-        
-//     } else {
-//         home.style.opacity = 1;
-//         homeImg.style.opacity = 0;
-//     }
-// });
-
+// HAMBURGER TOGGLE LOGIC
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navigation.classList.toggle('active');
     logo.classList.remove("active");
     hamburger.classList.toggle("active-menu");
     HEADER.classList.remove("active-header");
+    // REMOVE HAMBURGER ON LINK CLICK
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            hamburger.classList.remove('active');
+            navigation.classList.remove('active');
+            logo.classList.remove("active");
+            hamburger.classList.remove("active-menu");
+        });
+    })
 
 });
 
-// REMOVE HAMBURGER ON LINK CLICK
-const navLinks = document.querySelectorAll('.header a');
-console.log(navLinks);
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        hamburger.classList.remove('active');
-        navigation.classList.remove('active');
-        logo.classList.remove("active");
-        hamburger.classList.remove("active-menu");
-    });
-})
-
+// PAGE DATE
 date.innerHTML = new Date().getFullYear();
+
+
+// INTERSECTION OBSERVERS
+const home = document.getElementById("home");
+const main = document.getElementById("main");
+const homeBtn = document.querySelector(".top-btn");
+// console.log(HEADER);
+
+// MAIN SECTION OBSERVER
+const homeOptions = {
+    rootMargin: "-95% 0px 0px 0px"
+};
+
+const homeObserver = new IntersectionObserver(
+    function(entries , homeObserver) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                console.log("home NOT intersecting");
+                HEADER.classList.remove("active-header");
+            } else {
+                HEADER.classList.add("active-header");
+                console.log("home IS intersecting");
+            }
+        });
+    }, homeOptions 
+);
+
+homeObserver.observe(home);
+
+// MAIN SECTION OBSERVER
+const mainOptions = {
+    rootMargin: "0% 0px -85% 0px"
+};
+
+const mainObserver = new IntersectionObserver(
+    function(entries , mainObserver) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                console.log("main NOT intersecting");
+                HEADER.classList.add("hero-header");
+                homeBtn.classList.remove("active-btn");
+            } else {
+                HEADER.classList.remove("hero-header");
+                homeBtn.classList.add("active-btn");
+                console.log("main IS intersecting");
+            }
+        });
+    }, mainOptions 
+);
+
+mainObserver.observe(main);
